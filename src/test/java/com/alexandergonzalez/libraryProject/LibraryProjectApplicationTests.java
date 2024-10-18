7eb1530733e5d208385ec14b42087fab8a5441bd
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-17 20:22:02 -0600

    Inicio del proyecto: Estructura

---
+++ src/test/java/com/alexandergonzalez/libraryProject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandergonzalez.libraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
