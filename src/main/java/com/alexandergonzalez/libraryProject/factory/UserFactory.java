d0c2d8e56a2a2c5b973936db4e5fd62392351a12
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-18 13:12:12 -0600

    Se implementa una fabrica por entidad

---
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/UserFactory.java
@@ -0,0 +1,25 @@
+package com.alexandergonzalez.libraryProject.factory;
+
+import com.alexandergonzalez.libraryProject.repositories.user.UserJPARepository;
+import com.alexandergonzalez.libraryProject.services.user.UserJPAService;
+import com.alexandergonzalez.libraryProject.services.user.UserMongoService;
+
+public class UserFactory {
+
+    private final UserJPAService userJPAService;
+    private final UserMongoService userMongoService;
+
+    public UserFactory(UserJPAService userJPAService, UserMongoService userMongoService) {
+        this.userJPAService = userJPAService;
+        this.userMongoService = userMongoService;
+    }
+
+    public UserService getUserService(String type) {
+        if ("jpa".equalsIgnoreCase(type)) {
+            return userJPAService;
+        } else if ("mongo".equalsIgnoreCase(type)) {
+            return userMongoService;
+        }
+        throw new IllegalArgumentException("Invalid database type: " + type);
+    }
+}
