c154c90c3eb5d8aa071540b52977280b57b10171
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-19 21:28:20 -0600

    Cambio como se seleccionaba el tipo de base de datos

--- src/main/java/com/alexandergonzalez/libraryProject/factory/UserFactory.java
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/UserFactory.java
@@ -2,6 +2,8 @@ package com.alexandergonzalez.libraryProject.factory;
 
 import com.alexandergonzalez.libraryProject.services.user.UserJPAService;
 import com.alexandergonzalez.libraryProject.services.user.UserMongoService;
+import org.flywaydb.core.internal.database.DatabaseType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,18 +11,20 @@ public class UserFactory {
 
     private final UserJPAService userJPAService;
     private final UserMongoService userMongoService;
+    @Value("${databaseType}")
+    private String databaseType;
 
     public UserFactory(UserJPAService userJPAService, UserMongoService userMongoService) {
         this.userJPAService = userJPAService;
         this.userMongoService = userMongoService;
     }
 
-    public UserService getUserService(String type) {
-        if ("jpa".equalsIgnoreCase(type)) {
+    public UserService getUserService() {
+        if ("JPA".equalsIgnoreCase(databaseType)) {
             return userJPAService;
-        } else if ("mongo".equalsIgnoreCase(type)) {
+        } else if ("MONGO".equalsIgnoreCase(databaseType)) {
             return userMongoService;
         }
-        throw new IllegalArgumentException("Invalid database type: " + type);
+        throw new IllegalArgumentException("Invalid database type: " + databaseType);
     }
 }
