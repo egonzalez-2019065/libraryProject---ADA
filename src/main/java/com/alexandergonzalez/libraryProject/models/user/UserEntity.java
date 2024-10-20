47f992480d3790470fb18e123962e2ab68e1a860
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-20 09:37:34 -0600

    Cambio en el campo del id

--- src/main/java/com/alexandergonzalez/libraryProject/models/user/UserEntity.java
+++ src/main/java/com/alexandergonzalez/libraryProject/models/user/UserEntity.java
@@ -15,7 +15,7 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private String name;
     private String lastname;
