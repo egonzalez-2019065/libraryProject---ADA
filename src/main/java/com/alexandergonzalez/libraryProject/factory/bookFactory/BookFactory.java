83c02fc720b5519f96211bb552f3dfd116052031
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-20 09:37:19 -0600

    Se implementa la persistencia JPA

--- src/main/java/com/alexandergonzalez/libraryProject/factory/bookFactory/BookFactory.java
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/bookFactory/BookFactory.java
@@ -1,6 +1,7 @@
 package com.alexandergonzalez.libraryProject.factory.bookFactory;
 
 import com.alexandergonzalez.libraryProject.repositories.book.BookMongoRepository;
+import com.alexandergonzalez.libraryProject.services.user.BookJPAService;
 import com.alexandergonzalez.libraryProject.services.user.BookMongoService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,18 +10,20 @@ import org.springframework.stereotype.Component;
 public class BookFactory {
 
     private final BookMongoService bookMongoService;
+    private final BookJPAService bookJPAService;
 
     @Value("${databaseType}")
     private String databaseType;
 
-    public BookFactory(BookMongoService bookMongoService) {
+    public BookFactory(BookMongoService bookMongoService, BookJPAService bookJPAService) {
         this.bookMongoService = bookMongoService;
+        this.bookJPAService = bookJPAService;
     }
 
 
     public BookService getBookService(){
         if ("JPA".equalsIgnoreCase(databaseType)) {
-            return null;
+            return bookJPAService;
         } else if ("MONGO".equalsIgnoreCase(databaseType)) {
             return bookMongoService;
         }
