df8e9c8e2ccbf47d85bc3edf240e23c8d02278cf
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-20 23:28:12 -0600

    Cambios realizados para separa la lógica para las diferentes persistencias

--- src/main/java/com/alexandergonzalez/libraryProject/services/user/UserMongoService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/services/user/UserMongoService.java
@@ -3,13 +3,12 @@ package com.alexandergonzalez.libraryProject.services.user;
 import com.alexandergonzalez.libraryProject.dto.user.UserDto;
 import com.alexandergonzalez.libraryProject.factory.UserService;
 import com.alexandergonzalez.libraryProject.models.user.UserDocument;
+import com.alexandergonzalez.libraryProject.models.user.UserEntity;
 import com.alexandergonzalez.libraryProject.repositories.user.UserMongoRepository;
 import com.alexandergonzalez.libraryProject.utils.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service("mongoUserService")
 public class UserMongoService implements UserService {
@@ -42,5 +41,33 @@ public class UserMongoService implements UserService {
         user.setRole(Role.USER);
         userMongoRepository.save(user);
         return this.toDto(user);
+    }
+
+    @Override
+    public UserDocument findById(String id) {
+        UserDocument userFound = userMongoRepository.findById(id).orElse(null);
+        if(userFound != null){
+           return userFound;
+        }
+        return null;
+    }
+
+    @Override
+    public UserEntity findByIdJPA(Long id) {
+        return null;
+    }
+
+    @Override
+    public UserDto findByIdDtoJPA(Long id) {
+        return null;
+    }
+
+    @Override
+    public UserDto findByIdDto(String id){
+        UserDocument userFound = findById(id);
+        if(userFound != null){
+            return this.toDto(userFound);
+        }
+        return null;
     }
 }
