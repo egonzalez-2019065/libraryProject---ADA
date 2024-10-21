0cf03007f4b0b9bc25146e1d90db44c92aa3a886
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-20 23:28:15 -0600

    Cambios realizados para separa la lógica para las diferentes persistencias

--- src/main/java/com/alexandergonzalez/libraryProject/factory/UserService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/UserService.java
@@ -1,7 +1,18 @@
 package com.alexandergonzalez.libraryProject.factory;
 
 import com.alexandergonzalez.libraryProject.dto.user.UserDto;
+import com.alexandergonzalez.libraryProject.models.user.UserDocument;
+import com.alexandergonzalez.libraryProject.models.user.UserEntity;
 
 public interface UserService {
+    // Mongo
     UserDto saveUser(UserDto userDto);
+    UserDto findByIdDto(String id);
+    UserDocument findById(String id);
+    //JPA
+    UserEntity findByIdJPA(Long id);
+    UserDto findByIdDtoJPA(Long id);
+
+
+
 }
