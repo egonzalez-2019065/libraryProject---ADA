dc99612b5eb48cbfc625569c8984fcc61f2f23a6
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-20 23:24:52 -0600

    Métodos agregados para manejar una lógica diferente para cada persistencia

--- src/main/java/com/alexandergonzalez/libraryProject/factory/bookFactory/BookService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/bookFactory/BookService.java
@@ -1,7 +1,16 @@
 package com.alexandergonzalez.libraryProject.factory.bookFactory;
 
 import com.alexandergonzalez.libraryProject.dto.book.BookDto;
+import com.alexandergonzalez.libraryProject.models.book.BookDocument;
+import com.alexandergonzalez.libraryProject.models.book.BookEntity;
 
 public interface BookService {
+    // Mongo
     BookDto saveBook(BookDto bookDto);
+    BookDocument findById(String id);
+    BookDto findByIdDto(String id);
+    // JPA
+
+    BookEntity findByIdJPA(Long id);
+    BookDto findByIdDtoJPA(Long id);
 }
