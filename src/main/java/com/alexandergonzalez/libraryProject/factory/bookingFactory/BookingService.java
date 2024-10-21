1db5c516c6ae2b801e07581bd8ac852459e5a0e6
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-21 17:55:06 -0600

    Interfaz para la fabrica de Reservas

---
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/bookingFactory/BookingService.java
@@ -0,0 +1,8 @@
+package com.alexandergonzalez.libraryProject.factory.bookingFactory;
+
+import com.alexandergonzalez.libraryProject.dto.booking.BookingDto;
+
+public interface BookingService {
+
+    BookingDto saveBooking(BookingDto bookingDto);
+}
