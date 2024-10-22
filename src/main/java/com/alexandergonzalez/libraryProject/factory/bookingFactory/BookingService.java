42093abdbb5df247f63d24d641509f859b692e68
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-22 14:25:32 -0600

    Se agregan los métodos para listar las reservas

--- src/main/java/com/alexandergonzalez/libraryProject/factory/bookingFactory/BookingService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/bookingFactory/BookingService.java
@@ -2,7 +2,12 @@ package com.alexandergonzalez.libraryProject.factory.bookingFactory;
 
 import com.alexandergonzalez.libraryProject.dto.booking.BookingDto;
 
+import java.util.List;
+
 public interface BookingService {
 
     BookingDto saveBooking(BookingDto bookingDto);
+    List<BookingDto> getLoans();
+    List<BookingDto> findByUserId(String id);
+
 }
