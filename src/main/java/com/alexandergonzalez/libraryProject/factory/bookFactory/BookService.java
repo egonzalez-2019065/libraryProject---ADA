0781bae20474c85ce7f035a152bbd490051a3383
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-22 17:25:29 -0600

    Métodos implementados para completar el crud de libros

--- src/main/java/com/alexandergonzalez/libraryProject/factory/bookFactory/BookService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/bookFactory/BookService.java
@@ -4,16 +4,15 @@ import com.alexandergonzalez.libraryProject.dto.book.BookDto;
 import com.alexandergonzalez.libraryProject.models.book.BookDocument;
 import com.alexandergonzalez.libraryProject.models.book.BookEntity;
 
+import java.util.List;
+
 public interface BookService {
     // Mongo
     BookDto saveBook(BookDto bookDto);
     BookDocument findById(String id);
+    BookEntity findByIdJPA(Long id); // Método para encontrar por id para la persistencia JPA
     BookDto findByIdDto(String id);
     BookDto updateBook(String id, BookDto bookDto);
-    // JPA
-
-    BookEntity findByIdJPA(Long id);
-    BookDto findByIdDtoJPA(Long id);
-    BookDto updateBookJPA(Long id, BookDto bookDto);
-
+    List<BookDto> getBooks();
+    BookDto deleteBook(String id);
 }
