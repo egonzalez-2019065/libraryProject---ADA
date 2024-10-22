d792886389a019267cae9f59dc2cf8fd00f64929
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-22 00:13:19 -0600

    Validaciones agregadas para poder realizar un prestamo

--- src/main/java/com/alexandergonzalez/libraryProject/services/loan/LoanJPAService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/services/loan/LoanJPAService.java
@@ -4,27 +4,33 @@ import com.alexandergonzalez.libraryProject.dto.book.BookDto;
 import com.alexandergonzalez.libraryProject.dto.loan.LoanDto;
 import com.alexandergonzalez.libraryProject.factory.loanFactory.LoanService;
 import com.alexandergonzalez.libraryProject.models.book.BookEntity;
+import com.alexandergonzalez.libraryProject.models.booking.BookingEntity;
 import com.alexandergonzalez.libraryProject.models.loan.LoanEntity;
 import com.alexandergonzalez.libraryProject.models.user.UserEntity;
+import com.alexandergonzalez.libraryProject.repositories.booking.BookingJPARepository;
 import com.alexandergonzalez.libraryProject.repositories.loan.LoanJPARepository;
 import com.alexandergonzalez.libraryProject.services.book.BookJPAService;
 import com.alexandergonzalez.libraryProject.services.user.UserJPAService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 
 @Service("jpaLoanService")
 public class LoanJPAService implements LoanService {
 
     private final LoanJPARepository loanJPARepository;
+    private final BookingJPARepository bookingJPARepository;
     private final UserJPAService userJPAService;
     private final BookJPAService bookJPAService;
 
 
     @Autowired
-    public LoanJPAService(LoanJPARepository loanJPARepository, UserJPAService userJPAService, BookJPAService bookJPAService) {
+    public LoanJPAService(LoanJPARepository loanJPARepository, BookingJPARepository bookingJPARepository, UserJPAService userJPAService, BookJPAService bookJPAService) {
         this.loanJPARepository = loanJPARepository;
+        this.bookingJPARepository = bookingJPARepository;
         this.userJPAService = userJPAService;
         this.bookJPAService = bookJPAService;
     }
@@ -51,20 +57,39 @@ public class LoanJPAService implements LoanService {
     }
 
     @Override
-    public LoanDto saveLoan(LoanDto loanDto){
+    public LoanDto saveLoan(LoanDto loanDto) {
         LoanEntity loanEntity = new LoanEntity();
         UserEntity userDto = userJPAService.findByIdJPA(loanDto.getUserIdJPA());
         BookEntity bookDto = bookJPAService.findByIdJPA(loanDto.getBookIdJPA());
 
+        // Verificar si el usuario o el libro existen
+        if (userDto == null || bookDto == null) {
+            return null; // Retornar null si el usuario o el libro no existen
+        }
+
+        // Verificar si el libro está disponible
+        if (!bookDto.isAvailable()) {
+            return null; // Retornar null si el libro no está disponible
+        }
+
+        // Verificar si ya existe un préstamo activo para el usuario y el libro
+        LoanEntity existingLoan = loanJPARepository.findByUserEntity_IdAndBookEntity_IdAndStatusTrue(userDto.getId(), bookDto.getId()).orElse(null);
+        if (existingLoan != null) {
+            return null; // Retornar null si ya existe un préstamo activo
+        }
+
+        // Marcar el libro como no disponible
         bookDto.setAvailable(false);
         BookDto bookToDto = bookJPAService.toDto(bookDto);
-
         bookJPAService.updateBookJPA(bookToDto.getIdEntity(), bookToDto);
 
+        // Configurar los detalles del nuevo préstamo
         loanEntity.setUserEntity(userDto);
         loanEntity.setBookEntity(bookDto);
+
+        // Guardar el nuevo préstamo
         loanJPARepository.save(loanEntity);
-        return this.toDto(loanEntity);
+        return this.toDto(loanEntity); // Retornar el DTO del préstamo guardado
     }
 
     @Override
@@ -72,5 +97,60 @@ public class LoanJPAService implements LoanService {
         return loanJPARepository.findAll().stream()
                 .map(this::toDtoGet)
                 .toList();
+    }
+
+
+    @Override
+    public LoanDto returnBook(String loanId) {
+        LoanEntity loanFound = loanJPARepository.findById(Long.valueOf(loanId)).orElse(null);
+
+        if (loanFound != null) {
+            BookEntity foundBook = bookJPAService.findByIdJPA(loanFound.getBookEntity().getId());
+
+            if (foundBook != null) {
+                // Establecer la fecha de retorno y cambiar el estado del préstamo a falso
+                loanFound.setReturnDate(LocalDateTime.now());
+                loanFound.setStatus(false);
+                loanJPARepository.save(loanFound);
+
+                // Marcar el libro como disponible
+                foundBook.setAvailable(true);
+
+                // Verificar si hay reservas para este libro
+                List<BookingEntity> activeReservations = bookingJPARepository.findByBookEntity_IdAndStatusTrue(foundBook.getId());
+                if (!activeReservations.isEmpty()) {
+                    // Tomar la primera reserva (más antigua)
+                    BookingEntity firstReservation = activeReservations.stream()
+                            .min(Comparator.comparing(BookingEntity::getBookingAt))
+                            .orElse(null);
+
+                    if(firstReservation != null) {
+                        // Crear un nuevo préstamo para el usuario de la reserva
+                        LoanEntity newLoan = new LoanEntity();
+                        newLoan.setUserEntity(firstReservation.getUserEntity());
+                        newLoan.setBookEntity(foundBook);
+
+                        // Guardar el nuevo préstamo
+                        loanJPARepository.save(newLoan);
+
+                        // Opcional: puedes eliminar la reserva o marcarla como usada
+                        firstReservation.setStatus(false); // Marcar reserva como inactiva
+                        bookingJPARepository.save(firstReservation);
+
+                        // Nuevamente setear el estado del libro a no disponible
+                        foundBook.setAvailable(false);
+                    }
+                }
+
+                // Guardar los datos del libro
+                BookDto bookToDto = bookJPAService.toDto(foundBook);
+                bookJPAService.updateBookJPA(bookToDto.getIdEntity(), bookToDto);
+
+                // Retornar el DTO del préstamo
+                return this.toDto(loanFound);
+            }
+            return null;
+        }
+        return null;
     }
 }
--- src/main/java/com/alexandergonzalez/libraryProject/services/loan/LoanMongoService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/services/loan/LoanMongoService.java
@@ -2,31 +2,37 @@ package com.alexandergonzalez.libraryProject.services.loan;
 
 import com.alexandergonzalez.libraryProject.dto.book.BookDto;
 import com.alexandergonzalez.libraryProject.dto.loan.LoanDto;
-import com.alexandergonzalez.libraryProject.dto.user.UserDto;
 import com.alexandergonzalez.libraryProject.factory.loanFactory.LoanService;
 import com.alexandergonzalez.libraryProject.models.book.BookDocument;
+import com.alexandergonzalez.libraryProject.models.booking.BookingDocument;
 import com.alexandergonzalez.libraryProject.models.loan.LoanDocument;
 import com.alexandergonzalez.libraryProject.models.user.UserDocument;
+import com.alexandergonzalez.libraryProject.repositories.booking.BookingMongoRepository;
 import com.alexandergonzalez.libraryProject.repositories.loan.LoanMongoRepository;
 import com.alexandergonzalez.libraryProject.services.book.BookMongoService;
 import com.alexandergonzalez.libraryProject.services.user.UserMongoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 @Service("mongoLoanService")
 public class LoanMongoService implements LoanService {
 
     private final LoanMongoRepository loanMongoRepository;
+    private final BookingMongoRepository bookingMongoRepository;
     private final UserMongoService userMongoService;
     private final BookMongoService bookMongoService;
 
 
 
+
     @Autowired
-    public LoanMongoService(LoanMongoRepository loanMongoRepository, UserMongoService userMongoService, BookMongoService bookMongoService) {
+    public LoanMongoService(LoanMongoRepository loanMongoRepository, BookingMongoRepository bookingMongoRepository, UserMongoService userMongoService, BookMongoService bookMongoService) {
         this.loanMongoRepository = loanMongoRepository;
+        this.bookingMongoRepository = bookingMongoRepository;
         this.userMongoService = userMongoService;
         this.bookMongoService = bookMongoService;
     }
@@ -53,10 +59,24 @@ public class LoanMongoService implements LoanService {
     }
     
     @Override
-    public LoanDto saveLoan(LoanDto loanDto){
+    public LoanDto saveLoan(LoanDto loanDto) {
         LoanDocument loanDocument = new LoanDocument();
         UserDocument userDto = userMongoService.findById(loanDto.getUserId());
         BookDocument bookDto = bookMongoService.findById(loanDto.getBookId());
+
+        if (userDto == null || bookDto == null) {
+            return null;
+        }
+
+
+        if (!bookDto.isAvailable()) {
+            return null;
+        }
+
+        LoanDocument existingLoan = loanMongoRepository.findByUserDocumentAndBookDocumentAndStatusTrue(userDto.getId(), bookDto.getId()).orElse(null);
+        if (existingLoan != null) {
+            return null;
+        }
 
         bookDto.setAvailable(false);
         BookDto bookToDto = bookMongoService.toDto(bookDto);
@@ -75,4 +95,58 @@ public class LoanMongoService implements LoanService {
                 .map(this::toDtoGet)
                 .toList();
     }
+
+    @Override
+    public LoanDto returnBook(String loanId) {
+        LoanDocument loanFound = loanMongoRepository.findById(loanId).orElse(null);
+
+        System.out.println(loanFound);
+        if(loanFound != null){
+            BookDocument foundBook = bookMongoService.findById(loanFound.getBookDocument().getId());
+            System.out.println(foundBook);
+            if(foundBook != null){
+                // Setteamos los valores para marcar que el libro ha sido devuelto
+                loanFound.setReturnDate(LocalDateTime.now());
+                loanFound.setStatus(false);
+                loanMongoRepository.save(loanFound);
+
+                // Setteamos los valores para que el libro quede disponible
+                foundBook.setAvailable(true);
+
+                // Verificar si hay reservas para este libro
+                List<BookingDocument> activeReservations = bookingMongoRepository.findByBookDocumentAndStatusTrue(foundBook.getId());
+                if (!activeReservations.isEmpty()) {
+
+                    // Tomar la primera reserva
+                    BookingDocument firstReservation = activeReservations.get(0);
+
+                    // Crear un nuevo préstamo para el usuario de la reserva
+                    LoanDocument newLoan = new LoanDocument();
+                    newLoan.setUserDocument(firstReservation.getUserDocument());
+                    newLoan.setBookDocument(foundBook);
+
+                    // Guardar el nuevo préstamo
+                    loanMongoRepository.save(newLoan);
+
+                    // Opcional: puedes eliminar la reserva o marcarla como usada
+                    firstReservation.setStatus(false); // Marcar reserva como inactiva
+                    bookingMongoRepository.save(firstReservation);
+
+                    // Nuevamente setteamos los valores para que el libro no quede disponible
+                    foundBook.setAvailable(false);
+
+                }
+
+                // Guardamos los datos del libro ya sea quede disponible o no
+                BookDto bookToDto = bookMongoService.toDto(foundBook);
+                bookMongoService.updateBook(bookToDto.getIdDocument(), bookToDto);
+
+                // Retornamos el dto
+                return this.toDto(loanFound);
+            }
+            return null;
+        }
+        return null;
+    }
+    
 }
