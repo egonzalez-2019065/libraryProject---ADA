ce1253ecbcf2aca9e2cea4dfc4ded12191e5d9fb
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-21 20:43:20 -0600

    Se implementa la lógica para poder realizar reservas

--- src/main/java/com/alexandergonzalez/libraryProject/repositories/booking/BookingJPARepository.java
+++ src/main/java/com/alexandergonzalez/libraryProject/repositories/booking/BookingJPARepository.java
@@ -1,10 +1,15 @@
 package com.alexandergonzalez.libraryProject.repositories.booking;
 
 
+import com.alexandergonzalez.libraryProject.models.book.BookEntity;
 import com.alexandergonzalez.libraryProject.models.booking.BookingEntity;
+import com.alexandergonzalez.libraryProject.models.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookingJPARepository extends JpaRepository<BookingEntity, Long> {
+    Optional<BookingEntity> findByUserEntity_IdAndBookEntity_Id(Long userEntity,  Long bookEntity);
 }
