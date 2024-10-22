a00dec4ccf15fecd47acf41c550b86587f86bc6c
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-22 00:12:44 -0600

    Métodos agregados para realizar las diferentes busquedas

--- src/main/java/com/alexandergonzalez/libraryProject/repositories/loan/LoanJPARepository.java
+++ src/main/java/com/alexandergonzalez/libraryProject/repositories/loan/LoanJPARepository.java
@@ -1,9 +1,14 @@
 package com.alexandergonzalez.libraryProject.repositories.loan;
 
+import com.alexandergonzalez.libraryProject.models.loan.LoanDocument;
 import com.alexandergonzalez.libraryProject.models.loan.LoanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LoanJPARepository extends JpaRepository<LoanEntity, Long> {
+    Optional<LoanEntity> findByUserEntity_IdAndBookEntity_IdAndStatusTrue(Long user, Long book);
+
 }
--- src/main/java/com/alexandergonzalez/libraryProject/repositories/loan/LoanMongoRepository.java
+++ src/main/java/com/alexandergonzalez/libraryProject/repositories/loan/LoanMongoRepository.java
@@ -4,6 +4,11 @@ import com.alexandergonzalez.libraryProject.models.loan.LoanDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface LoanMongoRepository extends MongoRepository<LoanDocument, String> {
+    Optional<LoanDocument> findByUserDocumentAndBookDocumentAndStatusTrue(String user, String book);
+
 }
