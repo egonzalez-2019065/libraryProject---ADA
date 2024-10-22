6e77f9e02b7cbb0e4b2963aed82269a73a4577a6
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-22 00:11:48 -0600

    Método para devolver el libro

--- src/main/java/com/alexandergonzalez/libraryProject/controllers/LoanController.java
+++ src/main/java/com/alexandergonzalez/libraryProject/controllers/LoanController.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/v1/loan")
 public class LoanController {
@@ -23,22 +24,41 @@ public class LoanController {
 
     @PostMapping
     public Object saveLoan(@RequestBody LoanDto loanDto){
-        System.out.println(loanDto);
         LoanService loanService = loanFactory.getLoanService();
         HashMap<String, Object> response = new HashMap<>();
         LoanDto savedLoan = loanService.saveLoan(loanDto);
-        response.put("Libro prestado", savedLoan);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        if(savedLoan != null){
+            response.put("Libro prestado", savedLoan);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }
+        response.put("message", "No se pudo realizar el prestamo, verifica que el libro esté disponible");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @GetMapping()
     public ResponseEntity<Object> getAllLoans() {
         LoanService loanService = loanFactory.getLoanService();
-        List<LoanDto> loanDtos = loanService.getLoans();
+        List<LoanDto> loanDto = loanService.getLoans();
 
         Map<String, Object> response = new HashMap<>();
-        response.put("Préstamos encontrados", loanDtos);
-
+        response.put("Préstamos encontrados", loanDto);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
+    @PutMapping("/return/{id}")
+    public ResponseEntity<Object> returnBook(@PathVariable("id") String bookId) {
+        LoanService loanService = loanFactory.getLoanService();
+        HashMap<String, Object> response = new HashMap<>();
+
+        LoanDto returnedLoan = loanService.returnBook(bookId);
+        if (returnedLoan != null) {
+            response.put("Libro devuelto", returnedLoan);
+            return ResponseEntity.ok().body(response);
+        } else {
+            response.put("Error", "No se encontró el préstamo para este libro.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+    }
+
+
 }
