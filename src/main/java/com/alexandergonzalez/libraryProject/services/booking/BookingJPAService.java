cbde481db946b0560ccaf8437f11e69f10bb5b0b
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-21 20:43:40 -0600

    Se implementa la lógica para poder realizar reservas

--- src/main/java/com/alexandergonzalez/libraryProject/services/booking/BookingJPAService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/services/booking/BookingJPAService.java
@@ -3,20 +3,54 @@ package com.alexandergonzalez.libraryProject.services.booking;
 
 import com.alexandergonzalez.libraryProject.dto.booking.BookingDto;
 import com.alexandergonzalez.libraryProject.factory.bookingFactory.BookingService;
+import com.alexandergonzalez.libraryProject.models.book.BookEntity;
+import com.alexandergonzalez.libraryProject.models.booking.BookingEntity;
+import com.alexandergonzalez.libraryProject.models.user.UserEntity;
 import com.alexandergonzalez.libraryProject.repositories.booking.BookingJPARepository;
+import com.alexandergonzalez.libraryProject.services.book.BookJPAService;
+import com.alexandergonzalez.libraryProject.services.user.UserJPAService;
 import org.springframework.stereotype.Service;
 
 @Service("jpaBookingService")
 public class BookingJPAService implements BookingService {
 
     private final BookingJPARepository bookingJPARepository;
+    private final UserJPAService userJPAService;
+    private final BookJPAService bookJPAService;
 
-    public BookingJPAService(BookingJPARepository bookingJPARepository) {
+    public BookingJPAService(BookingJPARepository bookingJPARepository, UserJPAService userJPAService, BookJPAService bookJPAService) {
         this.bookingJPARepository = bookingJPARepository;
+        this.userJPAService = userJPAService;
+        this.bookJPAService = bookJPAService;
+    }
+
+    private BookingDto toDto(BookingEntity booking) {
+        return new BookingDto(
+                booking.getId(),
+                booking.getUserEntity().getId(),
+                booking.getBookEntity().getId(),
+                booking.getBookingAt(),
+                booking.getReturnDate()
+        );
     }
 
     @Override
     public BookingDto saveBooking(BookingDto bookingDto) {
+        BookingEntity bookingToSave = new BookingEntity();
+        UserEntity user = userJPAService.findByIdJPA(bookingDto.getUserIdJPA());
+        BookEntity book = bookJPAService.findByIdJPA(bookingDto.getBookIdJPA());
+
+        if (book.isAvailable()) {
+            BookingEntity existingBooking = bookingJPARepository.findByUserEntity_IdAndBookEntity_Id(user.getId(), book.getId()).orElse(null);
+
+            if (existingBooking == null) {
+                bookingToSave.setUserEntity(user);
+                bookingToSave.setBookEntity(book);
+                bookingJPARepository.save(bookingToSave);
+                return this.toDto(bookingToSave);
+            }
+            return null;
+        }
         return null;
     }
 }
