67bbd8a4b2f7d41a4a86584b069fad9b2e5c6d77
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-10-22 10:10:55 -0600

    Método agregado para mostrar préstamos por usuario

--- src/main/java/com/alexandergonzalez/libraryProject/repositories/loan/LoanJPARepository.java
+++ src/main/java/com/alexandergonzalez/libraryProject/repositories/loan/LoanJPARepository.java
@@ -5,10 +5,12 @@ import com.alexandergonzalez.libraryProject.models.loan.LoanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LoanJPARepository extends JpaRepository<LoanEntity, Long> {
     Optional<LoanEntity> findByUserEntity_IdAndBookEntity_IdAndStatusTrue(Long user, Long book);
+    List<LoanEntity> findByUserEntity_IdAndStatusTrue(Long userId);
 
 }
