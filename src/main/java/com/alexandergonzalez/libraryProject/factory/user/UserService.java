16d0748517885eff81d336b360237c67066b6ba2
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-23 07:51:51 -0600

    Métodos de la interfaz de usuarios para completar el crud de usuarios

--- src/main/java/com/alexandergonzalez/libraryProject/factory/user/UserService.java
+++ src/main/java/com/alexandergonzalez/libraryProject/factory/user/UserService.java
@@ -1,8 +1,11 @@
 package com.alexandergonzalez.libraryProject.factory.user;
 
+import com.alexandergonzalez.libraryProject.dto.RoleDto;
 import com.alexandergonzalez.libraryProject.dto.user.UserDto;
 import com.alexandergonzalez.libraryProject.models.user.UserDocument;
 import com.alexandergonzalez.libraryProject.models.user.UserEntity;
+
+import java.util.List;
 
 public interface UserService {
     // Mongo
@@ -10,5 +13,11 @@ public interface UserService {
     UserDto findByIdDto(String id);
     UserDocument findById(String id);
     UserEntity findByIdJPA(Long id);
+    UserDto findByUsername(String name);
+    UserDto updateUser(String id, UserDto userDto);
+    Boolean updateRole(String id, RoleDto roleDto, String userLogged);
+    UserDto deleteUser(String id);
+    List<UserDto> getUsers();
+
 
 }
