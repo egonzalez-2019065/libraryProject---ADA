404327b82f553248a3eab3edc097a1b9f098caca
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-23 07:50:03 -0600

    Nuevo constructor para el usuario por defecto

--- src/main/java/com/alexandergonzalez/libraryProject/dto/user/UserDto.java
+++ src/main/java/com/alexandergonzalez/libraryProject/dto/user/UserDto.java
@@ -22,22 +22,32 @@ public class UserDto {
     private String password;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private String whoUpdatedTo;
 
-    public UserDto(long idEntity, String name, String lastname, String username, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public UserDto(long idEntity, String name, String lastname, String username, LocalDateTime createdAt, LocalDateTime updatedAt, String whoUpdatedTo) {
         this.idEntity = idEntity;
         this.name = name;
         this.lastname = lastname;
         this.username = username;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.whoUpdatedTo = whoUpdatedTo;
     }
 
-    public UserDto(String idDocument, String name, String lastname, String username, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public UserDto(String idDocument, String name, String lastname, String username, LocalDateTime createdAt, LocalDateTime updatedAt, String whoUpdatedTo) {
         this.idDocument = idDocument;
         this.name = name;
         this.lastname = lastname;
         this.username = username;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.whoUpdatedTo = whoUpdatedTo;
+    }
+
+    public UserDto(String name, String lastname, String username, String password) {
+        this.name = name;
+        this.lastname = lastname;
+        this.username = username;
+        this.password = password;
     }
 }
