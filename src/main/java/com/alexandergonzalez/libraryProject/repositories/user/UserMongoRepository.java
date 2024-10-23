f8604e16a56d8b8293fd2d5eca385d566c40cd76
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-10-23 07:50:28 -0600

    Método para encontrar el usuario por su nombre de usuario

--- src/main/java/com/alexandergonzalez/libraryProject/repositories/user/UserMongoRepository.java
+++ src/main/java/com/alexandergonzalez/libraryProject/repositories/user/UserMongoRepository.java
@@ -1,10 +1,14 @@
 package com.alexandergonzalez.libraryProject.repositories.user;
 
+import com.alexandergonzalez.libraryProject.dto.user.UserDto;
 import com.alexandergonzalez.libraryProject.models.user.UserDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserMongoRepository extends MongoRepository<UserDocument, String> {
+    Optional<UserDocument> findByUsername(String name);
 
 }
